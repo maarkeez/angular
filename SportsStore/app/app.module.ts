6ba3ec9cc706069145c16430ce6405a99ab213e2
maarkeez <d.marquez.delgado.91@gmail.com>
2018-03-31 17:48:22 +0200

    [SportsStore] Created admin module

--- SportsStore/app/app.module.ts
+++ SportsStore/app/app.module.ts
@@ -28,8 +28,13 @@ import { StoreFirstGuard } from "./storeFirst.guard";
 			path: "checkout", component: CheckoutComponent,
 			canActivate: [StoreFirstGuard]
 		},
+		{
+			path: "admin",
+			loadChildren: "app/admin/admin.module#AdminModule",
+			canActivate: [StoreFirstGuard]
+		},
 		{ path: "**", redirectTo: "/store" }
-		])],
+	])],
 	providers: [StoreFirstGuard],
 	declarations: [AppComponent],
 	bootstrap: [AppComponent]
